fc881c321a34c3586fb8ab0340bd9ccea31cf780
Joey-art <joseph.mcintosh@opensesame.com>
2021-04-05 16:31:36 -0700

    new naeuro code

---
+++ walkinginsects.py
@@ -0,0 +1,8 @@
+import wikipedia
+Phasmatodea = wikipedia.page("Phasmatodea")
+print(Phasmatodea.summary)
+print(Phasmatodea.sections)
+print(len(Phasmatodea.sections))
+print(Phasmatodea.images)
+print(len(Phasmatodea.images))
+print(Phasmatodea.content)
